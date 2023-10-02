7600b692a6e2400b8e4a32e418cc130e33c03be0
Robin J <robin.j@commerceiq.ai>
2023-10-02 20:43:04 +0530

    css

--- src/pages/sideNav.tsx
+++ src/pages/sideNav.tsx
@@ -84,7 +84,8 @@ export default function SideNav(props: ISideNavProps) {
             </li>
             <li>
               <a
-                onClick={() => setid(2)}
+                onClick={() => {setid(2); localStorage.setItem("isAdmin", "false"); localStorage.setItem("isLoggedIn", "false");}}
+                href="/login"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <svg
